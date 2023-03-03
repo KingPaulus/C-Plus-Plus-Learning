16976e284989b26cbf3abb68358ace3c75a4564e
King Paulus <mergetp02@gmail.com>
2023-03-03 09:35:04 +0100

    Explain Exeptions and Interface

--- Exeption.cpp
+++ Exeption.cpp
@@ -1,5 +1,37 @@
-//
-// Created by Paul Merget on 03.03.23.
-//
+#include <iostream>
+#include <string>
+#include <stdexcept>
 
-#include "Exeption.h"
+using namespace std;
+
+
+class Mensch{
+private:
+    string name;
+    int alter;
+public:
+    Mensch (string name2, int alter2){
+        name = name2;
+        alter = alter2;
+    }
+    void Beispiel() {
+        if(alter == 40) {
+            alter = 41;
+        }
+        try {
+            int alter = alter + 1;
+
+            throw (alter);
+        }
+        catch(int i) {
+            cout << "Mein alter ist: ";
+            cout << i;
+        }
+    }
+};
+
+int main() {
+    Mensch J1 = *new Mensch("John", 40);
+    J1.Beispiel();
+    return 0;
+}
--- Interface.cpp
+++ Interface.cpp
@@ -1,5 +1,30 @@
-//
-// Created by Paul Merget on 03.03.23.
-//
+#include <iostream>
+#include <string>
+#include <stdexcept>
 
-#include "Interface.h"
+using namespace std;
+
+class verzeichnis {
+public:
+    virtual void Beispiel() = 0;
+};
+
+class Mensch : verzeichnis {
+private:
+    string name;
+    int alter;
+public:
+    Mensch (string name2, int alter2){
+        name = name2;
+        alter = alter2;
+    }
+    void Beispiel() {
+        cout << "Test";
+    }
+};
+
+int main() {
+    Mensch J1 = *new Mensch("John", 40);
+    J1.Beispiel();
+    return 0;
+}
